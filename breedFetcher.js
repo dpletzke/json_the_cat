c0b84494a4df3c56844770e3008e0f7f4f613382
Dan Pletzke <dpletzke@gmail.com>
2020-09-24 21:45:19 +0000

    breedfetcher error handling and module setup

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,33 +1,38 @@
 const request = require('request');
-const breedInput = process.argv.slice(2)[0];
 
-const urlSearch = 'https://api.thecatapi.com/v1/images/search?'; 
+
+const urlSearch = 'https://api.thecatapi.com/v1/images/search?';
 const urlSearchBreedsExt = 'breed_ids=';
 
 const urlBreeds = 'https://api.thecatapi.com/v1/breeds';
 
-  
-request(urlBreeds, (error, response, body) => {  
-  if(error) console.log(error); 
-  if(response.statusCode !== 200) console.log(response.statusCode);
+const fetchBreedDescription = function(breedName, callback) {
+  request(urlBreeds, (error, response, body) => {
+    if (error) callback(error, null);
 
-  const breedsArr = JSON.parse(body);
-  const breedCodeRef = {};
-  breedsArr.forEach(({ id, name }) => {
-    breedCodeRef[id] = name;
-  });  
-  const breedCode = Object.keys(breedCodeRef).find(key => breedCodeRef[key] === breedInput);
-  if(!breedCode) {
-    console.log('No such breed!');
-    return undefined;
-  }
+    /* create breeds reference object */
+    const breedsArr = JSON.parse(body);
+    const breedCodeRef = {};
+    breedsArr.forEach(({ id, name }) => {
+      breedCodeRef[id] = name;
+    });
 
-  const urlSearchBreed = urlSearch + urlSearchBreedsExt + breedCode;  
-  request(urlSearchBreed, (error, response, body) => {
-    if(error) console.log(error); 
-    if(response.statusCode !== 200) console.log(response.statusCode);
-
-    console.log(JSON.parse(body)[0].breeds[0].description);
+    /* find the breed code to be used in url from reference object and input */
+    const breedCode = Object.keys(breedCodeRef).find(key => breedCodeRef[key] === breedName);
+    if (!breedCode) {
+      callback('No such breed!', null);
+    } else {
+      /* create url to request from with breed code and breeds extension */
+      const urlSearchBreed = urlSearch + urlSearchBreedsExt + breedCode;
+      request(urlSearchBreed, (error, response, body) => {
+        if (error) callback(error, null);
+        else {
+          callback(null, JSON.parse(body)[0].breeds[0].description);
+        }
+      });
+    }
   });
-  
-});  
+};
+
+ 
+module.exports = { fetchBreedDescription };
